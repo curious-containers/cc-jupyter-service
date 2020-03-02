bc0bd6bfdea4afaac373b9e0090cb5df1716275d
Bluemi <bruno.schilling@protonmail.ch>
2020-03-02 17:06:39 +0100

    implemented base project

--- cc_jupyter_service/service/app.py
+++ cc_jupyter_service/service/app.py
@@ -1,22 +1,36 @@
-from argparse import ArgumentParser
+import os
 
 from flask import Flask
+import cc_jupyter_service.service.db as db
+import cc_jupyter_service.service.auth as auth
 
 DESCRIPTION = 'CC-Agency Broker.'
 
 
-def get_arguments():
-    parser = ArgumentParser(description=DESCRIPTION)
-    parser.add_argument(
-        '-c', '--conf-file', action='store', type=str, metavar='CONF_FILE',
-        help='CONF_FILE (yaml) as local path.'
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',  # TODO: overwrite dev
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-    return parser.parse_args()
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-app = Flask('cc-jupyter-service')
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
+    @app.route('/', methods=['GET'])
+    def get_root():
+        return b'Hello world!'
 
-@app.route('/', methods=['GET'])
-def get_root():
-    return "<span style='color:red'>I am app 3</span>"
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+
+    return app
